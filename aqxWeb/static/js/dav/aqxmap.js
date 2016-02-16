3f7d39bab35a9c59a3d45f1cf795ba0d34b54b91
muonictonic <briangi@github.ccs.neu.edu>
2016-02-15 20:21:11 -0800

    Naive implementation for filtering map markers

--- aqxWeb/static/js/dav/aqxmap.js
+++ aqxWeb/static/js/dav/aqxmap.js
@@ -24,6 +24,79 @@
 
 $(document).ready(function() {
 
+    var map;
+
+    ////Sample data
+    //function uploadClicked() {
+    //    //var systemInfo = json_ob;
+    //    var filteredSystems = [];
+    //
+    //    var dropDownObject1 = document.getElementById("selectTechnique");
+    //    var dropDownObject2 = document.getElementById("selectOrganism");
+    //    var dropDownObject3 = document.getElementById("selectCrop");
+    //    var dropDownObject4 = document.getElementById("selectGrowbedMedium");
+    //    var dp1 = dropDownObject1.value;
+    //    var dp2 = dropDownObject2.value;
+    //    var dp3 = dropDownObject3.value;
+    //    var dp4 = dropDownObject4.value;
+    //    console.log(dp1);
+    //    console.log(dp2);
+    //    console.log(dp3);
+    //    console.log(dp4);
+    //
+    //    if (dp1 != "Choose an Aquaponics Technique") {
+    //        filteredSystems = _.filter(json_ob, function (system) {
+    //            return (system.description.aqx_techniques === dp1);
+    //        });
+    //    } else {
+    //        filteredSystems = json_ob;
+    //    }
+    //
+    //    if (dp2 != "Choose an Aquatic Organism") {
+    //        filteredSystems = _.filter(filteredSystems, function (system) {
+    //            return (system.description.aqx_organism === dp2);
+    //        });
+    //    }
+    //
+    //    if (dp3 != "Choose a Crop") {
+    //        filteredSystems = _.filter(filteredSystems, function (system) {
+    //            return (system.description.crop === dp3);
+    //        });
+    //    }
+    //
+    //    if (dp4 != "Choose a Growbed Medium") {
+    //        filteredSystems = _.filter(filteredSystems, function (system) {
+    //            return (system.description.growbed_media === dp4);
+    //        });
+    //    }
+    //
+    //    function removeFilteredMarkers(filteredSystems){
+    //        for (var i = 0; i < markers.length; i++) {
+    //            for (var j = 0; j < filteredSystems.length; j++){
+    //                if (markers[i].title === filteredSystems[j].title){
+    //                    markers[i].setMap(null);
+    //                }
+    //            }
+    //        }
+    //    }
+    //    removeFilteredMarkers(filteredSystems);
+    //};
+    //
+    function populateDropdown(key, elementId){
+        var select = document.getElementById(elementId);
+        for(var i = 0; i < meta_data[key].length; i++) {
+            var opt = meta_data[key][i];
+            var el = document.createElement("option");
+            el.textContent = opt;
+            el.value = opt;
+            select.appendChild(el);
+        }
+    };
+    populateDropdown("aqx_techniques", "selectTechnique");
+    populateDropdown("aqx_organisms", "selectOrganism");
+    populateDropdown("growbed_media", "selectGrowbedMedium");
+    populateDropdown("crops", "selectCrop");
+
     function buildContentString(dataPoint) {
         return "<h2>" + dataPoint.title + "</h2>" +
             "<ul><li>Aquaponics Technique: " + dataPoint.description['aqx_techniques'] + "</li>" +
@@ -54,7 +127,24 @@ $(document).ready(function() {
                 infoWindow.close();
             }
         })(marker1));
+        markers.push(marker1);
     }
+
+    function setMapOnAll(map) {
+        for (var i = 0; i < markers.length; i++) {
+            markers[i].setMap(map);
+        }
+    }
+    //
+    //function removeFilteredMarkers(filteredSystems){
+    //    for (var i = 0; i < markers.length; i++) {
+    //        for (var j = 0; j < filteredSystems.length; j++){
+    //            if (markers[i].title === filteredSystems[j].title){
+    //                markers[i].setMap(null);
+    //            }
+    //        }
+    //    }
+    //}
 
     function initializeMap() {
         var myLatLng = {lat: 57.9, lng: 14.6};
