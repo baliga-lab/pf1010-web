ffae044d8d177778e4360669415402570556ef9c
kananin <kanani.n@husky.neu.edu>
2016-02-16 19:35:29 -0500

    Merged Home and profile
    Nisha and Jyothi merged first draft for home and profile page

--- aqxWeb/sc/models.py
+++ aqxWeb/sc/models.py
@@ -1,32 +1,103 @@
 from py2neo import authenticate,Graph, Node, Relationship
-from neo4jrestclient.client import GraphDatabase
+from passlib.handlers import bcrypt
+from datetime import datetime as dt
+import bcrypt
+import os
+import uuid
 
-authenticate("localhost:7474", "neo4j", "neo4jnew")
+url = os.environ.get('GRAPHENEDB_URL', 'http://localhost:7474')
+username = os.environ.get('NEO4J_USERNAME')
+password = os.environ.get('NEO4J_PASSWORD')
+
+
+if username and password:
+    authenticate(url.strip('http://'), username, password)
+
+
+#authenticate("localhost:7474", "neo4j", "neo4jnew")
 graph = Graph()
 
 class User:
 
-    def __init__(self, firstname, lastname, dateofbirth, organization, email):
-        self.db = GraphDatabase("http://localhost:7474", username="neo4j", password="neo4jnew")
+   # def __init__(self, firstname, lastname, dateofbirth, organization, email, username):
+
+    def __init__(self,username):
+        '''
         self.firstname = firstname
         self.lastname = lastname
         self.dateofbirth = dateofbirth
         self.organization = organization
-        self.email = email
+        self.email = email '''
+        self.username = username
 
     def find(self):
-        user_returned = graph.find_one("Users", "name", "Venkatesh")
-        return user_returned
+        user = graph.find_one("User", "username", self.username)
+        return user
 
-    def updateprofile(self):
-        user_to_update = self.find()
+    def updateprofile(self,firstname, lastname, dateofbirth, organization, email):
         query = """
         MATCH(x:Users)
         WHERE x.sql_id = 1
         SET x.name = {nameupdate}
         """
 
-        return graph.cypher.execute(query, nameupdate=self.firstname)
+        return graph.cypher.execute(query, nameupdate=firstname)
 
+
+    def verify_password(self, password):
+        user = self.find()
+        if user:
+            return True
+        else:
+            return False
+
+    def add_post(self, title, tags, text):
+        user = self.find()
+        post = Node(
+            "Post",
+            id=str(uuid.uuid4()),
+            title=title,
+            text=text,
+            timestamp=timestamp(),
+            date=date()
+        )
+        rel = Relationship(user, "PUBLISHED", post)
+        graph.create(rel)
+
+        tags = [x.strip() for x in tags.lower().split(',')]
+        for t in set(tags):
+            tag = graph.merge_one("Tag", "name", t)
+            rel = Relationship(tag, "TAGGED", post)
+            graph.create(rel)
+
+    def get_recent_posts(self):
+        query = """
+        MATCH (user:User)-[:PUBLISHED]->(post:Post)<-[:TAGGED]-(tag:Tag)
+        WHERE user.username = {username}
+        RETURN post, COLLECT(tag.name) AS tags
+        ORDER BY post.timestamp DESC LIMIT 5
+        """
+
+        return graph.cypher.execute(query, username=self.username)
+
+def get_todays_recent_posts():
+    query = """
+    MATCH (user:User)-[:PUBLISHED]->(post:Post)<-[:TAGGED]-(tag:Tag)
+    WHERE post.date = {today}
+    RETURN user.username AS username, post, COLLECT(tag.name) AS tags
+    ORDER BY post.timestamp DESC LIMIT 5
+    """
+
+    return graph.cypher.execute(query, today=date())
+
+
+def timestamp():
+    epoch = dt.utcfromtimestamp(0)
+    now = dt.now()
+    delta = now - epoch
+    return delta.total_seconds()
+
+def date():
+    return dt.now().strftime('%Y-%m-%d')
 
 
--- aqxWeb/sc/views.py
+++ aqxWeb/sc/views.py
@@ -1,10 +1,10 @@
 from flask import Flask, request, session, redirect, url_for, render_template, flash
-from models import User
+from models import User,get_todays_recent_posts
 from aqxWeb.sc import app
 
-@app.route('/')
-def index():
-    return render_template("index.html")
+@app.route('/profile')
+def profile():
+    return render_template("profile.html")
 
 @app.route('/updateprofile', methods=['GET', 'POST'])
 def updateprofile():
@@ -14,10 +14,50 @@ def updateprofile():
         dateofbirth = request.form['dob']
         organization = request.form['organization']
         email = request.form['email']
-        User(firstname, lastname, dateofbirth, organization, email).updateprofile()
+
+        User(session['username']).updateprofile(firstname, lastname, dateofbirth, organization, email)
         return "User Profile updated"
 
-'''
-if __name__ == '__main__':
-    app.run(debug=True)
-    '''
+@app.route('/')
+def index():
+    posts = get_todays_recent_posts()
+    return render_template('home.html', posts=posts)
+
+@app.route('/add_post', methods=['POST'])
+def add_post():
+    title = request.form['title']
+    tags = request.form['tags']
+    text = request.form['text']
+
+    if not title or not tags  or not text:
+        if not title:
+            flash('You must give your post a title.')
+        if not tags:
+            flash('You must give your post at least one tag.')
+        if not text:
+            flash('You must give your post a text body.')
+    else:
+        User(session['username']).add_post(title,tags, text)
+
+    return redirect(url_for('index'))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        if not User(username).verify_password(password):
+            flash('Invalid login.')
+        else:
+            session['username'] = username
+            flash('Logged in.')
+            return redirect(url_for('index'))
+
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    flash('Logged out.')
+    return redirect(url_for('index'))
