1c92dbf14eaecdb04439ecdc73befc97f4e1d3ea
Christoforus Juan Benvenuto <exquisense+bitbucket@gmail.com>
2016-03-07 01:45:16 -0800

    Fixed session

--- aqxWeb/frontend.py
+++ aqxWeb/frontend.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, session
 
 frontend = Blueprint('frontend', __name__, template_folder='templates',static_folder='static')
 
@@ -11,7 +11,6 @@ frontend = Blueprint('frontend', __name__, template_folder='templates',static_fo
 # returns: index.html page
 #######################################################################################
 def index():
-    user = request.cookies.get('user')
     return render_template('index.html')
 
 
@@ -24,17 +23,6 @@ def index():
 #######################################################################################
 def about():
     return render_template('about.html')
-
-
-@frontend.route('/systems')
-#######################################################################################
-# function : systems
-# purpose : renders list of all systems belonging to user (not yet finished making)
-# parameters : None
-# returns: systems.html with same exact format as system.html
-#######################################################################################
-def systems():
-    return render_template('systems.html')
 
 
 @frontend.route('/system')
@@ -58,18 +46,6 @@ def system():
 def settings():
     return render_template('settings.html')
 
-
-@frontend.route('/login', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if valid_login(request.form['username'],
-                       request.form['password']):
-            return log_the_user_in(request.form['username'])
-        else:
-            error = 'Invalid username/password'
-    # executed when request method is GET or when credentials are invalid
-    return render_template('login.html', error=error)
 
 
 @frontend.route('/coming')
