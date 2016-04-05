2b8dcc583b5417ef867499a55ddb2df699f6e492
Abhishek <abishek.ravichandran@gmail.com>
2016-04-04 21:10:17 -0400

    changes to ui code to use global connection pool, changed pool size to 4

--- aqxWeb/run.py
+++ aqxWeb/run.py
@@ -66,7 +66,7 @@ if __name__ == "__main__":
     app.config.from_envvar('AQUAPONICS_SETTINGS')
     create_conn(app)
     init_dav(pool)
-    ui.init_app(app)
+    ui.init_app(pool)
     # Intialize the social component global app instance
     init_sc_app(app)
     # Initialise UI's nav routing
