b7d4842891c4c365f75c1e2fffbb2f102b6927b3
Abhishek <abishek.ravichandran@gmail.com>
2016-04-08 14:43:52 -0400

    added logic for annotations

--- aqxWeb/stproc.sql
+++ aqxWeb/stproc.sql
@@ -5,7 +5,7 @@
 DELIMITER $$
 CREATE PROCEDURE `update_system_status` ()
 LANGUAGE sql
-COMMENT ''this procedure will run everyday at 12am to see if any of the system tables need to
+COMMENT 'this procedure will run everyday at 12am to see if any of the system tables need to
 be updated.
 Conditions for update:
 ------------------------------
@@ -30,13 +30,13 @@ Change status to pre-established
 	if no data is found for current_day - 90 days
 	OR
 	if events (remove_fish || remove_plants)
-Change status to terminated''
+Change status to terminated'
 BEGIN
 	declare sysid varchar(40);
 	declare ret_num int;
 	DECLARE done INT DEFAULT FALSE;
-	declare curr_state varchar(25);
-	DECLARE sys_cursor CURSOR FOR select system_uid, state from systems;
+	declare curr_state int(11);
+	DECLARE sys_cursor CURSOR FOR select system_uid, status from systems;
 	DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
 	open sys_cursor;
 	systems_loop: loop
@@ -48,37 +48,42 @@ BEGIN
 		END IF;
 
 		/*logic for pre-established systems*/
-		if curr_state = ''PRE-ESTABLISHED'' then
+		if curr_state = 100 then
 
 			call check_pre_established(sysid, ret_num);
 			if ret_num = 1 then
-				select "change to Established";
+				update systems set status = 200 where system_uid = sysid;
+				select "change to 200";
 			else
 				select "no change";
 			end if;
 		end if;
 
 		/*logic for established systems*/
-		if curr_state = ''ESTABLISHED'' then
+		if curr_state = 200 then
 			call check_established(sysid, ret_num);
 			if ret_num = 1 then
-				select "change to Suspended";
+				select "change to 300";
+				update systems set status = 300 where system_uid = sysid;
 			end if;
 			if ret_num = 2 then
-				select "change to pre-established";
+				select "change to 100";
+				update systems set status = 100 where system_uid = sysid;
 			end if;
 		end if;
 
 		/*logic for suspended systems*/
-		if curr_state = ''SUSPENDED'' then
+		if curr_state = 300 then
 
 			call check_suspended(sysid, ret_num);
 
 			if ret_num = 1 then
-				select "change to pre-established";
+				select "change to 100";
+				update systems set status = 100 where system_uid = sysid;
 			end if;
 			if ret_num = 2 then
-				select "change to Terminated";
+				select "change to 400";
+				update systems set status = 400 where system_uid = sysid;
 			end if;
 		end if;
 
@@ -87,7 +92,8 @@ BEGIN
 	close sys_cursor;
 END;
 
-DELIMITER $$
+$$
+
 CREATE PROCEDURE `check_pre_established` (sysid varchar(40), OUT ret int)
 COMMENT 'this procedure checks pre-established systems
 -- check if there atleast 7 readings for the previous 14 days and
@@ -163,6 +169,7 @@ BEGIN
 	declare ammonium_num90 int;
 	declare nitrite_num90 int;
 	declare nitrate_num90 int;
+	declare annotation_check int;
 	set date_90_days = (select DATE_SUB(CURDATE(), INTERVAL 90 DAY) as date from dual);
 	set date_1_days = (select DATE_SUB(CURDATE(), INTERVAL 1 DAY) as date from dual);
 	/*check ammonium values*/
@@ -188,8 +195,12 @@ BEGIN
 		select "no change";
 		set ret = 0;
 	end if;
+	/*logic to check for remove_plant or remove_fish, if yes set ret to 2*/
+	call check_annotations(sysid, annotation_check);
+	if annotation_check > 0 then
+		set ret = 2;
+	end if;
 
-	/*logic to check for remove_plant and remove_fish, if yes set ret to 2*/
 END
 
 $$
@@ -238,3 +249,34 @@ BEGIN
 	end if;
 
 END
+
+$$
+
+CREATE PROCEDURE `check_annotations` (sysid varchar(40), OUT ret int)
+BEGIN
+
+	set @GetSystem = 'select count(*) from
+	system_annotations a,
+	systems b where
+	a.system_id = b.id and
+	b.system_uid = ? and
+	(annotation_id = 7
+	or annotation_id = 9) into @num';
+	set @sysid = sysid;
+	prepare stmt from @GetSystem;
+	execute stmt using @sysid;
+	set ret = @num;
+
+END
+
+$$
+
+CREATE procedure `get_measurement_count` (tab varchar(100), pdate date, OUT num int)
+BEGIN
+	set @GetSystem = concat('select count(*) from ',tab
+			,' where time between ? and CURDATE() into @num');
+	set @pdate = pdate;
+	prepare stmt from @GetSystem;
+	execute stmt using @pdate;
+	set num = @num;
+END;
