bb1561b4cae4593b2f8c46f5f9d60f3239d0ef4c
briangillespie <briangi@github.ccs.neu.edu>
2016-04-14 12:00:36 -0700

    Touching up explore page

--- aqxWeb/dav/static/js/aqxmap.js
+++ aqxWeb/dav/static/js/aqxmap.js
@@ -254,6 +254,8 @@ function main(system_and_info_object) {
 
     $('#alert_placeholder').empty();
 
+    $("#selectStatus option[text='pre-established']").attr("selected","selected");
+
     $('#analyzeSystem').chosen({
         max_selected_options: MAX_SYSTEM_SELECTED,
         no_results_text: "Oops, nothing found!",
