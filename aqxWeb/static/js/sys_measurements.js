571aecc5973cb155c0330167b3ea8e1992c878f6
Verena <chung.v@husky.neu.edu>
2016-04-14 17:03:16 -0700

    Implementing "input_measurements" -- just need API routing fix

--- aqxWeb/static/js/sys_measurements.js
+++ aqxWeb/static/js/sys_measurements.js
@@ -1,24 +1,21 @@
 "use strict";
 
-var app = angular.module("aqx");
+var app = angular.module('aqx');
 
 app.controller('MeasurementController', function ($scope, $http) {
 
     $scope.addMeasurement = function (measure) {
+        $scope.message = false;
+
         function onSuccess(response) {
-            console.log(repsonse);
+            console.log(response);
+            $scope.message = true;
         }
         function onFailure(error) {
             console.log(error);
         }
-        var measurement = {
-                'system_uid': response.data.systemUID,
-                'measurement_id': response.data.measurement_id,
-                'time': response.data.time,
-                'value': response.data.value
-            };
         console.log(measure);
 
-        $http.post('/aqxapi/v1/measurements', measurement).then(onSuccess, onFailure);
+        $http.post('dav/aqxapi/v1/measurements', measure).then(onSuccess, onFailure);
     }
 });
