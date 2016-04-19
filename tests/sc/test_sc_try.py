72c84f85481071e5dccf3c9179cf898482ae3ca6
kananin <kanani.n@husky.neu.edu>
2016-04-18 20:01:47 -0400

    Added few for test cases

--- tests/sc/test_sc_try.py
+++ tests/sc/test_sc_try.py
@@ -25,21 +25,58 @@ class FlaskTestCase(unittest.TestCase):
         with self.app as client :
             with client.session_transaction() as session:
                 session['uid']=33
-                print(session)
-                print(run.app.config)
             res=client.get('/social/pendingRequest')
-            print(client)
+            print(res.data)
 
     @patch('flask.templating._render', return_value='Send_friend_request_works_fine')
     def test_send_friend_request(self,mocked):
         with self.app as client :
             with client.session_transaction() as session:
                 session['uid']=33
-                print(session)
-                print(run.app.config)
-
             res=client.post('/social/send_friend_request/30')
-            print(client)
+            print(res.data)
+
+    @patch('flask.templating._render', return_value='Block a friend works fine')
+    def test_block_friend(self,mocked):
+        with self.app as client :
+            with client.session_transaction() as session:
+                session['uid']=33
+            res=client.post('/social/block_friend/25')
+            print(res.data)
+
+    @patch('flask.templating._render', return_value='UnBlock a friend works fine')
+    def test_un_block_friend(self,mocked):
+        with self.app as client :
+            with client.session_transaction() as session:
+                session['uid']=33
+            res=client.post('/social/unblock_friend/25')
+            print(res.data)
+
+    @patch('flask.templating._render', return_value='View my friends work fine')
+    def test_un_block_friend(self,mocked):
+        with self.app as client :
+            with client.session_transaction() as session:
+                session['uid']=33
+            res=client.get('/social/friends')
+            print(res.data)
+
+    @patch('flask.templating._render', return_value='Search for friends work fine')
+    def test_search_friend(self,mocked):
+        with self.app as client :
+            with client.session_transaction() as session:
+                session['uid']=33
+            res=client.get('/social/searchFriends')
+            print(res.data)
+
+    @patch('flask.templating._render', return_value='Get groups work fine')
+    def test_get_groups(self,mocked):
+        with self.app as client :
+            with client.session_transaction() as session:
+                session['uid']=33
+            res=client.get('/social/groups')
+            print(res.data)
+
+
 
 
 
