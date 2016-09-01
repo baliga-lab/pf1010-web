bd3a24f5a7ee719a7dddfb56e7ef8097de3147ad
Brian Gillespie <bng1290@gmail.com>
2016-09-01 15:34:01 -0700

    chaging the interpolator used by angular so that we can use Jinja2 and Angular expressions in out HTML

--- aqxWeb/static/js/app.js
+++ aqxWeb/static/js/app.js
@@ -1,5 +1,13 @@
 var app = angular.module('aqx', []);
 
+
+// Flask's Jinja2 template engine takes precedence over angular's expressions
+// Angular and Jinja2 both use {{ }} for expressions, so we can actually convert the "interpolator"
+// symbol from:
+// {{ -> {a
+// and
+// }} -> a}
+// Now variables in an Angular controller's scope can be referenced in HTML using {a a}
 app.config(['$interpolateProvider', function($interpolateProvider) {
   $interpolateProvider.startSymbol('{a');
   $interpolateProvider.endSymbol('a}');
