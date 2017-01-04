5c16e7dc6621f4a6a4600518a25a5ce8cad303f9
Wei-ju Wu <weiju.wu@gmail.com>
2017-01-04 13:43:44 -0800

    old test suite removed
    to build a test suite that we can actually use for CI

--- setup.py
+++ setup.py
@@ -1,13 +1,9 @@
 from distutils.core import setup
 
 setup(
-    name='aqxWeb-NEU',
+    name='pf1010-web',
     version='',
-    packages=['tests', 'tests.sc', 'tests.sc.app', 'tests.sc.dao', 'tests.ui', 'tests.dav', 'tests.dav.app',
-              'tests.dav.dao', 'tests.integration', 'aqxWeb', 'aqxWeb.sc', 'aqxWeb.sc.app', 'aqxWeb.sc.dao',
-              'aqxWeb.sc.static', 'aqxWeb.sc.static.js', 'aqxWeb.sc.static.css', 'aqxWeb.sc.static.fonts',
-              'aqxWeb.sc.static.images', 'aqxWeb.app', 'aqxWeb.dao', 'aqxWeb.dav', 'aqxWeb.dav.app', 'aqxWeb.dav.dao',
-              'aqxWeb.dav.static.css', 'aqxWeb.dav.static.fonts', 'aqxWeb.dav.static.images'],
+    packages=[],
     url='',
     license='',
     author='Neu',
