7f7a86b085342250ab61f9dc839103d98d55714d
Wei-ju Wu <weiju.wu@gmail.com>
2017-01-10 18:51:31 -0800

    added dynamic number of fish and plant types

--- aqxWeb/static/js/create_system.js
+++ aqxWeb/static/js/create_system.js
@@ -1,12 +1,14 @@
 "use strict";
 
+/*
+ * Javascript support code for edit/create system forms. This includes AJAX form handling
+ * as well as dynamic forms.
+ */
 var aqx_editsystem;
 if (!aqx_editsystem) {
     aqx_editsystem = {};
 }
 
-// This is our form controller based solely on jQuery instead of
-// Angular 1
 (function () {
 
     aqx_editsystem.create = function() {
@@ -54,11 +56,65 @@ if (!aqx_editsystem) {
         });
     };
 
+    /*
+     * Build select boxes dynamically, so we can allow multiple e.g. fish and plant
+     * inputs in the input form
+     */
+    aqx_editsystem.make_select = function(selector, name, choices, initialEmpty) {
+        var s = $('<select>').attr('name', name).addClass('form-control');
+        if (initialEmpty) s.append($('<option>').attr('value', 0).text(""));
+        $.each(choices, function (i, e) {
+            s.append($('<option>').attr('value', e.id).text(e.name));
+        });
+        $(selector).replaceWith(s);
+    };
+
+    function makeInputRow(divID, typeName, num, choices, placeholder) {
+        var selectID = 'select_' + typeName + '_' + num;
+        var input = $('<input type="number" min="0" />')
+            .addClass('form-control')
+            .attr('name', typeName + 'Count')
+            .attr('placeholder', placeholder);
+        var newrow = $('<div>').addClass('form-group')
+            .append($('<div>').addClass('row')
+                    .append($('<div>').addClass('col-xs-8')
+                            .append($('<div id="' + selectID + '">'))
+                            .add($('<div>')
+                                 .addClass('col-xs-4')
+                                 .append(input))));
+        $('#' + divID).replaceWith(newrow.add('<div id="' + divID + '"></div>'));
+        aqx_editsystem.make_select('#' + selectID, typeName + 'ID_' + num, choices, true);
+    }
+
+    var numCropLists = 1;
+    var numOrganismLists = 1;
+    var MAX_LIST_LEN = 3;
+
     $(document).ready(function() {
         $('#get_geocoords').click(function () {
             address_to_geocode($("input[name='address']").val());
         });
         $('#create_form').submit(aqx_editsystem.create);
+        aqx_editsystem.make_select('#select_crops', 'cropID', crops, false);
+        aqx_editsystem.make_select('#select_organism', 'organismID', organisms, false);
+        $('#addcrop').click(function () {
+            if (numCropLists <= MAX_LIST_LEN) {
+                makeInputRow('newcrop', 'crop', numCropLists++, crops,
+                             'Number of Crops');
+                if (numCropLists == MAX_LIST_LEN) {
+                    $('#addcrop').remove();
+                }
+            }
+        });
+        $('#addorganism').click(function () {
+            if (numOrganismLists <= MAX_LIST_LEN) {
+                makeInputRow('neworganism', 'organism', numOrganismLists++, organisms,
+                             'Number of Organisms');
+                if (numOrganismLists == MAX_LIST_LEN) {
+                    $('#addorganism').remove();
+                }
+            }
+        });
     });
 
 }());
